b9993f5f8868820b22b23ead3d324b3ad2540fb4
Amit Kumar Sahni <sahniamitkumar2017@gmail.com>
2023-07-29 22:46:04 +0530

    added functionality to authenticate user

---
+++ models.py
@@ -0,0 +1,28 @@
+from typing import Optional
+from pydantic import BaseModel
+
+
+# Todo: Complete the camel case
+class CamelModel(BaseModel):
+    """
+    Converts to camel model
+    """
+    # class Config:
+    #     alias_generator = to_came_case
+    #     allow_population_by_field_name = True
+    pass
+
+
+class UserDetailsSchema(BaseModel):
+    name: str
+    password: str
+    existingPassword: Optional[str] = None
+
+
+class UserSignInDetails(UserDetailsSchema):
+    pass
+
+
+class UserResponse(BaseModel):
+    userName: str
+    userId: int
